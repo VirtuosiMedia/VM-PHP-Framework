5ff4b1be918a9aafe8f1c933f4a86e5bf5c94259
Virtuosi Media <contact@virtuosimedia.com>
2011-11-18 02:54:32 -0800

    Updated for the new database factory class

--- Includes/Vm/Db/File/Generator.php
+++ Includes/Vm/Db/File/Generator.php
@@ -1,18 +1,21 @@
 <?php
 /**
-* @author Virtuosi Media Inc.
-* @license: MIT License
-* Description: Generates a database file
-* Requirements: PHP 5.2 or higher
-*/
-class Vm_Db_File_Generator extends DbOperations {
+ * @author Virtuosi Media Inc.
+ * @license MIT License
+ * @description Generates a database file
+ * @requires PHP 5.2 or higher
+ * @extends Vm_Db_Ddl
+ * @uses Vm_Db_Exception
+ */
+class Vm_Db_File_Generator extends Vm_Db_Ddl {
 	
 	/**
-	 * Generates a database class file for the given table
+	 * @description Generates a database class file for the given table
+	 * @param string $dbType - The type of database
 	 * @param string $tableName - The name of the table
 	 * @param string $dir - The relative directory path, omitting the trailing forward slash '/'
 	 */
-	public function generate($tableName, $dir){
+	public function generate($dbType, $tableName, $dir){
 		$newtableName = str_replace('_', ' ', $tableName);
 		$newtableName = str_replace(' ', '', ucwords($newtableName));
 		$columns = $this->showColumns($tableName);
@@ -24,15 +27,34 @@ class Vm_Db_File_Generator extends DbOperations {
 		
 		$fileName = $dir.'/'.$newtableName.'.php';
 		$objectName = 'Db_'.$newtableName;
+
+		switch(strtolower($dbType)){
+			case 'mysql':
+				$dbType = 'MySQL';
+				$extension = 'Vm_Db_MySql_Dml';
+				break;
+			default:
+				throw new Vm_Db_Exception('Database driver type "'.$driverType.'" is not supported.');
+		}
 		
 		$file = <<<EOT
 <?php
-class $objectName extends DBObject {
-	function __construct(\$db, \$prefix = NULL) {
-		parent::__construct(\$db, '$tableName', array($fields), 'public', \$prefix);
+/**
+ * @description The database class for the $tableName table. Uses a $dbType database. This class has been auto-generated.
+ * @extends Vm_Db_Factory_Dml
+ * @extends $extension
+ */
+class $objectName extends Vm_Db_Factory_Dml {
+	
+	/**
+	 * @param object \$db - The PDO connection
+	 * @param string \$prefix - optional - The prefix for the database 
+	 * @return Returns the current object for chaining purposes
+	 */
+	function __construct(PDO \$db, \$prefix = NULL) {
+		parent::__construct(\$db, '$dbType', '$tableName', array($fields), 'public', \$prefix);
   	}
 }
-?>
 EOT;
 		$fileHandler = fopen($fileName, 'w');
 		fwrite($fileHandler, $file);
@@ -40,14 +62,14 @@ EOT;
 	}
 	
 	/**
-	 * Generates a database class file for all tables in the database
+	 * @description Generates a database class file for all tables in the database
+	 * @param string $dbType - The type of database
 	 * @param string $dir - The relative directory path, omitting the trailing forward slash '/'
 	 */
-	public function generateAll($dir){
+	public function generateAll($dbType, $dir){
 		$tables = $this->showTables();
 		foreach ($tables as $tableName){
-			$this->generate($tableName[0], $dir);	
+			$this->generate($dbType, $tableName[0], $dir);	
 		}		
 	}
 }
-?>
