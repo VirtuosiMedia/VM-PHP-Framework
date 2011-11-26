5354050155ce921b07dd731a3d8abeb551d6e32f
Virtuosi Media <contact@virtuosimedia.com>
2011-11-26 12:41:15 -0800

    Initial import

---
+++ Includes/Vm/Mixer/Exception.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Virtuosi Media Inc.
+ * @license: MIT License
+ * Description: An exception handler for VM_Mixer
+ * Requirements: PHP 5.2 or higher
+ */
+class Vm_Mixer_Exception extends Vm_Exception {}
+?>
