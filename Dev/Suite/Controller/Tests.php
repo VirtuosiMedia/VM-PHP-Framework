d530e7936fd0cf709fb10d878d9b0a1284c6115f
Virtuosi Media <contact@virtuosimedia.com>
2011-12-08 01:36:09 -0800

    Updated to include a view for individual tests

--- Dev/Suite/Controller/Tests.php
+++ Dev/Suite/Controller/Tests.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @author Virtuosi Media Inc.
- * @license: MIT License
- * @description: The test suite page controller for the VM PHP Framework Suite.
- * @requirements: PHP 5.2 or higher
+ * @license MIT License
+ * @description The test suite page controller for the VM PHP Framework Suite.
+ * @requirements PHP 5.2 or higher
  */
 class Suite_Controller_Tests extends Vm_Controller {
 	
@@ -11,8 +11,8 @@ class Suite_Controller_Tests extends Vm_Controller {
 	protected $settings;
 	
 	/**
-	 * 
-	 * @param array $params - An associative array of the URL parameters, with the parameter name as the key, it's value as the value
+	 * @param array $params - An associative array of the URL parameters, with the parameter name as the key, it's value 
+	 * 		as the value
 	 * @param array $settings - An associative settings array, with the setting name as the key, it's value as the value
 	 */
 	function __construct(array $params, array $settings){
@@ -20,11 +20,13 @@ class Suite_Controller_Tests extends Vm_Controller {
 		$this->settings = $settings;
 	}
 	
-	public function load(){
+	public function load(){	
 		$topNav = new Suite_Model_TopNav($this->params, $this->settings);
-		$testForm = new Suite_Model_Tests_Form($this->params, $this->settings);
-		$view = new Vm_View($this->defaultPath, $this->overridePath);
-				
+		$testSuite = new Suite_Model_Tests_Suite($this->settings);
+		$testForm = new Suite_Model_Tests_Form($this->params, $this->settings, $testSuite->getSuite());
+		$testSuite->setForm($testForm->getForm());
+		
+		$view = new Vm_View($this->defaultPath, $this->overridePath);		
 		$view->setViewspace('Header');
 		$view->pageTitle = 'VM PHP Framework Test Suite';
 		$view->scripts = array(
@@ -45,17 +47,27 @@ class Suite_Controller_Tests extends Vm_Controller {
 		$view->map($topNav->getViewData());
 		$view->loadTemplate('TopNav.php');
 		
-		$view->setViewspace('Body');
+		$view->setViewspace('Test Form');
 		$view->removeFilters(array('StripTags'));
+		$view->map($testForm->getViewData());	
+		$view->loadTemplate('Tests.php');
+	
+		if ($testSuite->displayResults()){
+			$view->setViewspace('Summary');
+			$testSuite->run();
+			$view->removeFilters(array('StripTags'));
+			$view->map($testSuite->getViewData());
+			$view->loadTemplate('Tests/Summary.php');
+			$view->loadTemplate('Tests/Tests.php');
+		}
 		
+		$view->setViewspace('Footer');
+		$view->removeFilters(array('StripTags'));
 		$version = new Vm_Version();
 		$view->version = $version->get('version');
 		$view->copyright = $version->get('copyright');			
-		
-		$view->map($testForm->getViewData());
-		$view->loadTemplate('Tests.php');
 		$view->loadTemplate('Footer.php');
-
-		$this->setView($view->render(array('Header', 'TopNav', 'Body')));
-	}
+		
+		$this->setView($view->render(array('Header', 'TopNav', 'Test Form', 'Summary', 'Results', 'Footer')));
+	}	
 }
