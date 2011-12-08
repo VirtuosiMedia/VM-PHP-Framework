2bbc83d49c316f0a6f1973f90a78df96fe7f90ef
Virtuosi Media <contact@virtuosimedia.com>
2011-12-08 01:34:48 -0800

    Initial import fixes #2

---
+++ Dev/Suite/Model/Tests/Suite.php
@@ -0,0 +1,265 @@
+<?php
+/**
+ * @author Virtuosi Media Inc.
+ * @license MIT License
+ * @description Loads the test suite data
+ */
+class Suite_Model_Tests_Suite extends Vm_Model {
+
+	protected $authors = array();
+	protected $coverage;
+	protected $filterData = array();
+	protected $formFilterData = array();
+	protected $form;
+	protected $groups = array();
+	protected $history;
+	protected $includeCoverage = FALSE;
+	protected $includeMetrics =  FALSE;
+	protected $metrics;
+	protected $results;
+	protected $saveResults = FALSE;
+	protected $settings;
+	protected $subgroups = array();
+	protected $testResults = array();
+	protected $testSuite;
+	
+	/**
+	 * @param array $settings - The framework settings array. 
+	 */
+	function __construct(array $settings){
+		$this->settings = $settings;
+		$this->loadTestSuite();	
+	}
+
+	protected function loadTestSuite(){
+		if ((isset($_POST['reports']))&&($_POST['reports'] == 'new')){
+			$this->includeCoverage = (isset($_POST['coverage'])) ? TRUE : FALSE;
+			$this->includeMetrics = (isset($_POST['metrics'])) ? TRUE : FALSE;
+		} else {
+			$this->loadReport();
+		}
+		
+		$this->saveResults = (isset($_POST['saveResults'])) ? TRUE : FALSE;
+		$this->testSuite = new Test_Suite('../Includes/Vm', 'Tests/Vm', $this->includeCoverage, $this->includeMetrics, $this->saveResults);		
+	}
+
+	protected function loadReport(){
+		if ((isset($_POST['reports']))&&(preg_match('/^[a-zA-Z0-9-.]*$/', $_POST['reports']))){
+			if (file_exists('Test/Reports/Suite/'.$_POST['reports'])){
+				$report = file('Test/Reports/Suite/'.$_POST['reports'], FILE_IGNORE_NEW_LINES);
+				$this->filterData = json_decode($report[0], TRUE);
+				$this->includeCoverage = $this->filterData['includeCoverage'];
+				$this->includeMetrics = $this->filterData['includeMetrics'];
+			}			
+		} 
+	}
+
+	/**
+	 * @description Sets the filter data, either from the form or a report
+	 */
+	protected function setFilterData(){
+		$data = ((isset($_POST['reports']))&&($_POST['reports'] == 'new')) ? $this->formFilterData : $this->filterData;	
+		if (sizeof($data['includedAuthors']) > 0){
+			$this->testSuite->setAuthors($data['includedAuthors']);
+		} else if (sizeof($data['excludedAuthors']) > 0){
+			$this->testSuite->setExcludedAuthors($data['excludedAuthors']);
+		}
+		
+		if (sizeof($data['includedGroups']) > 0){
+			$this->testSuite->setGroups($data['includedGroups']);
+		} else if (sizeof($data['excludedGroups']) > 0){
+			$this->testSuite->setExcludedGroups($data['excludedGroups']);
+		}
+
+		if (sizeof($data['includedSubgroups']) > 0){
+			$this->testSuite->setSubgroups($data['includedSubgroups']);
+		} else if (sizeof($data['excludedSubgroups']) > 0){
+			$this->testSuite->setExcludedSubgroups($data['excludedSubgroups']);
+		}
+	}	
+	
+	protected function saveResults(){
+		$results = array_merge($this->results->getViewData(), $this->coverage->getViewData(), $this->metrics->getViewData());	
+		$saveResults = new Test_Save_Suite($results, $this->includeCoverage, $this->includeMetrics);
+		$saveResults->setAuthors($this->testSuite->getAuthors());
+		$saveResults->setExcludedAuthors($this->testSuite->getExcludedAuthors());
+		$saveResults->setGroups($this->testSuite->getGroups());
+		$saveResults->setExcludedGroups($this->testSuite->getExcludedGroups());
+		$saveResults->setSubgroups($this->testSuite->getSubgroups());
+		$saveResults->setExcludedSubgroups($this->testSuite->getExcludedSubgroups());
+		$saveResults->save();		
+	}
+
+	protected function processForm(){
+		$isNewReport = ((!isset($_POST['saveResults']))&&((!isset($_POST['reports']))||(isset($_POST['reports']))&&($_POST['reports'] == 'new'))) ? TRUE : FALSE;
+		if (($isNewReport)||((isset($_POST['saveResults']))&&($_POST['reports'] == 'new'))){
+			$excludedAuthors = array();
+			$excludedGroups = array();
+			$excludedSubgroups = array();
+			$includedAuthors = array();
+			$includedGroups = array();
+			$includedSubgroups = array();
+
+			$include = ($this->form->getValue('include') == 'include') ? TRUE : FALSE;
+			
+			foreach ($this->authors as $author){
+				$authorString = 'author-'.preg_replace('#[^a-zA-Z_0-9]#', '', $author);
+				if ($this->form->getValue($authorString)){
+					if ($include){
+						$includedAuthors[] = $author;
+					} else {
+						$excludedAuthors[] = $author;
+					}
+				}
+			}
+
+			foreach ($this->groups as $group){
+				$groupString = 'group-'.preg_replace('#[^a-zA-Z_0-9]#', '', $group);
+				if ($this->form->getValue($groupString)){
+					if ($include){
+						$includedGroups[] = $group;
+					} else {
+						$excludedGroups[] = $group;
+					}
+				}
+			}				
+			
+			foreach ($this->subgroups as $subgroup){
+				$subgroupString = 'subgroup-'.preg_replace('#[^a-zA-Z_0-9]#', '', $subgroup);
+				if ($this->form->getValue($subgroupString)){
+					if ($include){
+						$includedSubgroups[] = $subgroup;
+					} else {
+						$excludedSubgroups[] = $subgroup;
+					}
+				}
+			}
+			
+			//Reverse the filters if any are marked unspecified
+			if (in_array('Unspecified', $includedAuthors)){
+				$excludedAuthors = array_diff($this->authors, $includedAuthors);
+				$includedAuthors = array();
+			} else if (in_array('Unspecified', $excludedAuthors)){
+				$includedAuthors = array_diff($this->authors, $excludedAuthors);
+				$excludedAuthors = array();
+			}
+
+			if (in_array('Unspecified', $includedGroups)){
+				$excludedGroups = array_diff($this->groups, $includedGroups);
+				$includedGroups = array();
+			} else if (in_array('Unspecified', $excludedGroups)){
+				$includedGroups = array_diff($this->groups, $excludedGroups);
+				$excludedGroups = array();
+			}
+
+			if (in_array('Unspecified', $includedSubgroups)){
+				$excludedSubgroups = array_diff($this->subgroups, $includedSubgroups);
+				$includedSubgroups = array();
+			} else if (in_array('Unspecified', $excludedSubgroups)){
+				$includedSubgroups = array_diff($this->subgroups, $excludedSubgroups);
+				$excludedSubgroups = array();
+			} 			
+
+			$this->formFilterData = array(
+				'includedAuthors'=>$includedAuthors,
+				'excludedAuthors'=>$excludedAuthors,
+				'includedGroups'=>$includedGroups,
+				'excludedGroups'=>$excludedGroups,
+				'includedSubgroups'=>$includedSubgroups,
+				'excludedSubgroups'=>$excludedSubgroups,						
+			);
+		}	
+	}
+
+	/**
+	 * @description Sets the form object so the suite has access to its data.
+	 * @param Test_SelectionForm $form
+	 */
+	public function setForm(Test_SelectionForm $form){
+		$this->authors = $form->getAuthors();
+		$this->groups = $form->getGroups();
+		$this->subgroups = $form->getSubgroups();
+		$this->form = $form->getForm();
+	}
+	
+	/**
+	 * @description Runs the tests
+	 * @return mixed - The rendered test results if they should be displayed, otherwise NULL
+	 */	
+	public function run(){
+		if ($this->displayResults()){
+			$this->processForm();
+			$this->setFilterData();
+			$this->testSuite->runAllTests();
+			$this->testResults = $this->testSuite->getResults();
+			$this->compileData();
+		} 
+	}
+	
+	/**
+	 * @description Compiles the results of all selected tests
+	 */	
+	protected function compileData(){
+		$testResults = array();
+		$testData = $this->testSuite->getTestData();
+		$testedClasses = $this->testSuite->getTestedClasses();
+		$coverage = $this->testSuite->getCoverage();
+		
+		foreach ($this->testResults as $name=>$results){
+			$test = new Suite_Model_Tests_Tests(
+				$testedClasses[$name]['name'], 
+				$testData[$name], 
+				$results, 
+				array($this->includeCoverage, $this->includeMetrics), 
+				$coverage[$name], 
+				$this->settings, 
+				$this->saveResults
+			);
+		
+			$testResults[$testedClasses[$name]['name']] = $test->getResults();
+		}
+		
+		$this->results = new Test_Model_Suite_Results($testResults);
+		$includeData = $this->results->getViewData(); 
+		
+		if ($this->includeCoverage){
+			$this->coverage = new Test_Model_Suite_Coverage($testResults);
+			$includeData = array_merge($includeData, $this->coverage->getViewData());
+		}
+		
+		if ($this->includeMetrics){
+			$this->metrics = new Test_Model_Suite_Metrics($testResults);
+			$includeData = array_merge($includeData, $this->metrics->getViewData());
+		}
+
+		if (isset($_POST['saveResults'])){			
+			$this->saveResults();
+		}			
+		
+		$this->history = new Test_Model_Suite_History();
+		$includeData = array_merge($includeData, $this->history->getViewData());				
+		
+		$this->setData('tests', $testResults);
+		$this->setData('includeMetrics', $this->includeMetrics);
+		$this->setData('includeCoverage', $this->includeCoverage);
+		
+		foreach ($includeData as $var=>$value){
+			$this->setData($var, $value);
+		}
+	}
+		
+	/**
+	 * @description Checks to see if the form has been submitted and if test results should be displayed.
+	 * @return Returns boolean. TRUE if the test results should be displayed, FALSE otherwise.
+	 */
+	public function displayResults(){
+		return (($this->form->submitted()) && (!$this->form->errorsExist())) ? TRUE : FALSE;
+	}
+	
+	/**
+	 * @return string - The rendered test suite and results
+	 */
+	public function getSuite(){
+		return $this->testSuite;
+	}	
+}
---
+++ Dev/Suite/Model/Tests/Tests.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * @author Virtuosi Media Inc.
+ * @license MIT License
+ * @description Models the results of an individual unit test case
+ */
+class Suite_Model_Tests_Tests extends Vm_Model {
+	
+	protected $allData = array();
+	protected $coverage;
+	protected $coverageResults;	
+	protected $file;
+	protected $fileExists;
+	protected $history;
+	protected $includeCoverage = FALSE;
+	protected $includeMetrics = TRUE;
+	protected $metrics;
+	protected $parents = array();
+	protected $results;
+	protected $saveResults = FALSE;
+	protected $settings;
+	protected $testData;
+	protected $testedClassName;
+	protected $testResults;
+	
+	/**
+	 * @param string $testedClassName - The name of the class being tested
+	 * @param array $testData - The metadata of the test class: array(author=>authorName, group=>groupName, 
+	 * 		subgroup=>subgroupName, description=>description)
+	 * @param array $testResults - The results of the test: array(name=subtestName, pass=bool, profile=testTime)
+	 * @param array $inclusions - An array of boolean values, with the first being whether or not to include test 
+	 * 		coverage, the second if metrics should be included in the test results.
+	 * @param array $coverageResults - The xdebug coverage stats array
+	 * @param array $settings - The framework settings array. 
+	 * @param boolean $saveResults - Whether or not to save the test data
+	 */
+	function __construct(
+		$testedClassName, 
+		array $testData, 
+		array $testResults,
+		array $inclusions, 
+		array $coverageResults = array(), 
+		array $settings,
+		$saveResults = FALSE
+	){
+		$this->testedClassName = $testedClassName;
+		$this->testData = $testData;
+		$this->testResults = $testResults;
+		$this->includeCoverage = $inclusions[0];
+		$this->includeMetrics = $inclusions[1];
+		$this->coverageResults = $coverageResults;
+		$this->settings = $settings;
+		$this->saveResults = $saveResults;
+		$this->getFile();
+		$this->compileTestData();
+	}
+	
+	protected function getFile(){
+		$fileName = '../Includes/'.str_replace('_', '/', $this->testedClassName).'.php';
+		if (file_exists($fileName)){
+			$this->file = file($fileName, FILE_IGNORE_NEW_LINES);
+			$this->fileExists = TRUE;
+
+		} else {
+			$this->file = array();
+			$this->fileExists = FALSE;
+		}		
+	}
+		
+	/**
+	 * @description Compiles the test results, coverage, metrics and history
+	 */
+	protected function compileTestData(){
+		$this->results = new Test_Model_Results(
+			$this->testedClassName, 
+			$this->testData, 
+			$this->testResults, 
+			$this->settings
+		);
+		$this->allData = array_merge($this->testData, $this->results->getData());
+		
+		if ($this->includeCoverage){
+			$this->coverage = new Test_Model_Coverage($this->testedClassName, $this->file, $this->coverageResults);
+			$this->allData = array_merge($this->allData, $this->coverage->getData());
+		}
+		
+		if ($this->includeMetrics){
+			$coveredMethods = ($this->includeCoverage) ? $this->coverage->getCoveredMethods() : array();
+			$this->metrics = new Test_Model_Metrics($this->testedClassName, $this->file, $coveredMethods);
+			$this->allData = array_merge($this->allData, $this->metrics->getData());
+		}
+		
+		if ($this->saveResults){
+			$save = new Test_Save_Test(
+				$this->testedClassName, 
+				$this->file, 
+				$this->allData, 
+				$this->includeCoverage, 
+				$this->includeMetrics
+			);
+		}
+		
+		$this->history = new Test_Model_History($this->testedClassName);
+		$this->allData = array_merge($this->allData, $this->history->getData());
+	}
+
+	/**
+	 * @return An associative array with all of the test results data from unit testing, coverage, and metrics, 
+	 * 		if all have been run
+	 */
+	public function getResults(){
+		return $this->allData;
+	}
+}
