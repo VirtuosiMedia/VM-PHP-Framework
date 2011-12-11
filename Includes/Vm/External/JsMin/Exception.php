8a26e21fc72eaa9ea90d2df3b324567aaacb8397
Virtuosi Media <contact@virtuosimedia.com>
2011-12-10 22:08:34 -0800

    Separated out from the JsMin class

---
+++ Includes/Vm/External/JsMin/Exception.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Virtuosi Media Inc.
+ * @license MIT License
+ * @description An exception handler for Vm\External\JsMin
+ * @requires PHP 5.2 or higher
+ * @extends Vm\Exception
+ * @namespace Vm\External\JsMin
+ */
+namespace Vm\External\JsMin;
+
+class Exception extends \Vm\Exception {}
