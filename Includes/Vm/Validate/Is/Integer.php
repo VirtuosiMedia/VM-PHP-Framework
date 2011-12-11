66233cf4a0ac205fa4305d184663161687d1b89b
Virtuosi Media <contact@virtuosimedia.com>
2011-12-10 16:47:40 -0800

    Removed Is directory and renamed files and classes because of namespace naming conflicts with reserved words

--- Includes/Vm/Validate/Is/Integer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* @author Virtuosi Media Inc.
-* @license: MIT License
-* Description: An alias for the Validate_Is_Int class
-* Requirements: PHP 5.2 or higher
-*/
-class Vm_Validate_Is_Integer extends Vm_Validate_Is_Int{}
-?>
--- Includes/Vm/Validate/Is/Real.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* @author Virtuosi Media Inc.
-* @license: MIT License
-* Description: An alias for the Validate_Is_Float class
-* Requirements: PHP 5.2 or higher
-*/
-class Vm_Validate_Is_Real extends Vm_Validate_Is_Float{}
-?>
