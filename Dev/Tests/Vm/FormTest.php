fa8d4991a4328c4838a86c8d42454599c76cd049
Virtuosi Media <contact@virtuosimedia.com>
2011-12-27 06:19:36 -0800

    Added two tests for Vm\Form

--- Dev/Tests/Vm/FormTest.php
+++ Dev/Tests/Vm/FormTest.php
@@ -10,6 +10,29 @@
  */
 namespace Tests\Vm;
 
+use Vm\Form;
+
 class FormTest extends \Tests\Test {
-			
+
+	protected function testFormNoAddedElements(){
+		$form = new Form(array('action'=>'test.php'));
+		$formOutput = <<<TESTOUTPUT
+<form method="post" action="test.php"><input value="TRUE" type="hidden" id="" name="submitted" />\n</form>\n
+TESTOUTPUT;
+		return $this->assertEqual(htmlspecialchars($form->render()), htmlspecialchars($formOutput));
+	}
+	
+	protected function testTextInputWithLabel(){
+		$form = new Form(array('action'=>'test.php'));
+		$form->text('testText', array(
+			'label'=>array(
+				'innerHtml'=>'Test Text'
+		)));
+		$formOutput = '<form method="post" action="test.php">'.
+			"<label for=\"testText\">Test Text</label>\n".
+			"<input type=\"text\" id=\"testText\" name=\"testText\" value=\"\" />\n".
+			"<input value=\"TRUE\" type=\"hidden\" id=\"\" name=\"submitted\" />\n".
+			"</form>\n";
+		return $this->assertEqual(htmlspecialchars(htmlspecialchars($form->render())), htmlspecialchars(htmlspecialchars($formOutput)));
+	}	
 }
