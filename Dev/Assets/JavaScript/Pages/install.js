c3b0bd603155b2d56990c887ffbdac0a86f10e5e
Virtuosi Media <contact@virtuosimedia.com>
2012-02-05 20:26:51 -0800

    Hide help, with trigger

--- Dev/Assets/JavaScript/Pages/install.js
+++ Dev/Assets/JavaScript/Pages/install.js
@@ -2,5 +2,12 @@
  * Handles all of the interactions for the install page
  */
 window.addEvent('domready', function(){
-	var tabs = new SimpleTabs($$('.tab'), {activeClassName: 'active', triggerEvent: 'click'});
+	$('help').setStyle('display', 'none');
+	$('helpLink').addEvent('click', function(){
+		if ($('help').getStyle('display') == 'none'){
+			$('help').setStyle('display', 'block');
+		} else {
+			$('help').setStyle('display', 'none');
+		}
+	});
 });
