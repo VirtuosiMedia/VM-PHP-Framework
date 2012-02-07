0794ee51fa01e0b55921fe078994665c01e301c3
Virtuosi Media <contact@virtuosimedia.com>
2012-02-06 21:37:19 -0800

    Added help dialog

--- Dev/Assets/JavaScript/Pages/install.js
+++ Dev/Assets/JavaScript/Pages/install.js
@@ -2,12 +2,5 @@
  * Handles all of the interactions for the install page
  */
 window.addEvent('domready', function(){
-	$('help').setStyle('display', 'none');
-	$('helpLink').addEvent('click', function(){
-		if ($('help').getStyle('display') == 'none'){
-			$('help').setStyle('display', 'block');
-		} else {
-			$('help').setStyle('display', 'none');
-		}
-	});
+	var modal = new InlineModal('helpLink', 'helpTitle', 'help');	
 });
