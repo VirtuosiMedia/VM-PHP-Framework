356b0a62610bb84e5d1b1a68090086488eae3312
Virtuosi Media <contact@virtuosimedia.com>
2012-02-07 21:55:13 -0800

    Renamed ids for consistency

--- Dev/Suite/View/Default/TopNav.php
+++ Dev/Suite/View/Default/TopNav.php
@@ -1,7 +1,9 @@
-<nav id="navContainer">
-	<ul id="topNav">
-		<?php foreach ($this->menu as $item): ?>
-			<li><a<?php echo $item['href'].$item['id'].$item['class']; ?>><?php echo $item['page'];?></a></li>
-		<?php endforeach; ?>		
-	</ul>
-</nav>
+<div id="topNavWrapper">
+	<nav id="topNavContainer">
+		<ul id="topNav">
+			<?php foreach ($this->menu as $item): ?>
+				<li><a<?php echo $item['href'].$item['id'].$item['class']; ?>><?php echo $item['page'];?></a></li>
+			<?php endforeach; ?>		
+		</ul>
+	</nav>
+</div>
