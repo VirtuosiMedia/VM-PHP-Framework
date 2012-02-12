c6a0a81cabd6490336a2338c1b91d2c303c6363a
Virtuosi Media <contact@virtuosimedia.com>
2012-02-12 01:00:20 -0800

    Added a container for the copyright

--- Dev/Suite/View/Default/Footer.php
+++ Dev/Suite/View/Default/Footer.php
@@ -1,3 +1,5 @@
-<p class="copyright">Version <?php echo $this->version; ?> - <?php echo $this->copyright; ?></p>
+<div id="copyrightContainer">
+	<p class="copyright">Version <?php echo $this->version; ?> - <?php echo $this->copyright; ?></p>
+</div>
 </body>
 </html>
