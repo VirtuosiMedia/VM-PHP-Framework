16d2ca118960b8198f50b8015bc6f430bb6c6632
Virtuosi Media <contact@virtuosimedia.com>
2012-02-12 00:23:58 -0800

    Added checks for unique and foreign keys

--- Includes/Vm/Db/FromXml.php
+++ Includes/Vm/Db/FromXml.php
@@ -15,70 +15,76 @@ class FromXml extends \Vm\Db\Factory\Ddl {
 	 */
 	protected function installStructure($table){
 		foreach ($table->structure->columns->column as $column){
-				if ((string) $column->extra == 'auto_increment'){
-					$this->setPrimaryKey((string)$column['name']);
-				} else {
-					switch (strtolower((string) $column['datatype'])) {
-						case 'smallint':
-							$this->addSmallInt((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'int':
-							$this->addInt((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;	
-						case 'bigint':
-							$this->addBigInt((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'float':
-							$this->addReal((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'real':
-							$this->addReal((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;							
-						case 'double':
-							$this->addDouble((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'decimal':
-							$this->addDecimal((string) $column['name'], (string) $column->value1, (string) $column->value2, (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'numeric':
-							$this->addNumeric((string) $column['name'], (string) $column->value1, (string) $column->value2, (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'char':
-							$this->addChar((string) $column['name'], (string) $column->value1, (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'varchar':
-							$this->addVarchar((string) $column['name'], (string) $column->value1, (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'text':
-							$this->addText((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'date':
-							$this->addDate((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'timestamp':
-							$this->addTimestamp((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-						case 'blob':
-							$this->addBlob((string) $column['name'], (string) $column->value1, (string) $column['null'], (string) $column->default, (string) $column->comment);
-							break;
-					}
+			if ((string) $column->extra == 'auto_increment'){
+				$this->setPrimaryKey((string)$column['name']);
+			} else {
+				switch (strtolower((string) $column['datatype'])) {
+					case 'smallint':
+						$this->addSmallInt((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'int':
+						$this->addInt((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;	
+					case 'bigint':
+						$this->addBigInt((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'float':
+						$this->addReal((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'real':
+						$this->addReal((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;							
+					case 'double':
+						$this->addDouble((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'decimal':
+						$this->addDecimal((string) $column['name'], (string) $column->value1, (string) $column->value2, (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'numeric':
+						$this->addNumeric((string) $column['name'], (string) $column->value1, (string) $column->value2, (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'char':
+						$this->addChar((string) $column['name'], (string) $column->value1, (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'varchar':
+						$this->addVarchar((string) $column['name'], (string) $column->value1, (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'text':
+						$this->addText((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'date':
+						$this->addDate((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'timestamp':
+						$this->addTimestamp((string) $column['name'], (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
+					case 'blob':
+						$this->addBlob((string) $column['name'], (string) $column->value1, (string) $column['null'], (string) $column->default, (string) $column->comment);
+						break;
 				}
 			}
-			
-			$uniqueKeys = array();
-			$foreignKeys = array();
-			foreach ($table->structure->keys->key as $key){
-				if ((string) $key['type'] == 'unique'){
-					$uniqueKeys[] = (string) $key;
-				} else if ((string) $key['type'] == 'foreign'){
-					$foreignKeys[] = (string) $key;
-				}
+		}
+		
+		$uniqueKeys = array();
+		$foreignKeys = array();
+		
+		foreach ($table->structure->keys->key as $key){
+			if ((string) $key['type'] == 'unique'){
+				$uniqueKeys[] = (string) $key;
+			} else if ((string) $key['type'] == 'foreign'){
+				$foreignKeys[] = (string) $key;
 			}
+		}
+		
+		if (sizeof($uniqueKeys) > 0){
 			$this->unique($uniqueKeys);
+		}
+		if (sizeof($foreignKeys) > 0){
 			$this->foreignKey($foreignKeys);
-			
-			//TODO: Add Table comments, collation
-			$this->createTable((string)$table['name'], (string)$table['schema']);		
+		}
+		
+		//TODO: Add Table comments, collation
+		$this->createTable((string)$table['name'], (string)$table['schema']);		
 	}
 	
 	/**
@@ -99,7 +105,7 @@ class FromXml extends \Vm\Db\Factory\Ddl {
 		foreach ($columns as $column){
 			$fields[] = $column['Field'];
 		}
-		$db = new DbObject($this->db, (string)$table['name'], $fields, (string)$table['schema'], $this->prefix);
+		$db = new \Vm\Db\Factory\Dml($this->db, $this->driverType, (string)$table['name'], $fields, (string)$table['schema'], $this->prefix);
 		foreach($fields as $field){
 			$db->$field = $data[$field];
 		}
@@ -107,7 +113,7 @@ class FromXml extends \Vm\Db\Factory\Ddl {
 	}
 	
 	/**
-	 * @description Creates a database table based on an xml database file
+	 * @description Creates a database table based on an XML database file
 	 * @param mixed $fileNames - The name of the xml database file with relative path included as a string, else an array
 	 * 	of file names and paths
 	 * @param string $mode - optional - 'structure', 'data', or 'both', defaults to 'both' 
@@ -115,7 +121,7 @@ class FromXml extends \Vm\Db\Factory\Ddl {
 	public function install($fileNames, $mode='both'){
 		$fileNames = (is_array($fileNames)) ? $fileNames : array($fileNames);
 		foreach ($fileNames as $fileName){
-			$xml = new SimpleXMLElement($fileName, NULL, TRUE);
+			$xml = new \SimpleXMLElement($fileName, NULL, TRUE);
 			foreach ($xml->table as $table) {
 				if ((strtolower($mode) == 'structure')||(strtolower($mode) == 'both')){
 					$this->installStructure($table);
