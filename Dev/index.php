163ddca29c31d82b2d1a1846888ca6acdf91bd31
Virtuosi Media <contact@virtuosimedia.com>
2012-02-20 21:49:17 -0800

    Added redirect to installation

--- Dev/index.php
+++ Dev/index.php
@@ -4,6 +4,11 @@ error_reporting(E_ALL);
 
 require_once('Assets/Autoload.php');
 
+if (!file_exists('Suite/Config.php')){
+	$url = new Vm\Url();
+	$url->redirect('install.php');
+}
+
 $settings = array(
 	'excludeFoldersFromDocs'=>array('Vm'),
 	'fluxCapacitor'=>FALSE,
