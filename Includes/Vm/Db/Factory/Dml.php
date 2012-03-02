e18540399484b682893ecb662c440c1c905ed4a5
Virtuosi Media <contact@virtuosimedia.com>
2012-03-02 00:05:26 -0800

    Added Active Record-like methods

--- Includes/Vm/Db/Factory/Dml.php
+++ Includes/Vm/Db/Factory/Dml.php
@@ -17,13 +17,13 @@ class Dml {
 	/**
 	 * @param object $db - The PDO database connection object
 	 * @param $driverType - The type of database driver: ie, mysql
-	 * @param string $table - The name of the table
+	 * @param array $table - The plural (actual) name of the table as the key, the singular name as the value
 	 * @param array $fields - The names of each field in the table
 	 * @param string $schema - The schema
 	 * @param $prefix - optional - A prefix for the table
 	 * @return The object for chaining 
 	 */
-	public function __construct(\PDO $db, $driverType, $table, array $fields, $schema, $prefix = NULL){
+	public function __construct(\PDO $db, $driverType, array $table, array $fields, $schema, $prefix = NULL){
 		$this->driverType = strtolower($driverType);
 		switch ($this->driverType){
 			case 'mysql':
@@ -53,6 +53,31 @@ class Dml {
 		$this->driver->$key = $value;
 	}
 
+	/**
+	 * @description Gets the plural form of the table name. This should correspond to the actual name of the table in
+	 * 		the database.
+	 * return The plural name of the table as a string.
+	 */
+	public function getPluralName(){
+		return $this->driver->getPluralName();
+	}
+	
+	/**
+	 * @description Gets the singular form of the table name.
+	 * return The singular name of the table as a string.
+	 */
+	public function getSingularName(){
+		return $this->driver->getSingularName();
+	}	
+	
+	/**
+	 * @description Gets the fields (columns) for the table
+	 * @return An array of the table fields.
+	 */
+	public function getFields(){
+		return $this->driver->getFields();
+	}	
+	
 	/**
 	 * @return string - The database driver type, i.e. 'mysql', 'oracle', etc.
 	 */
@@ -107,64 +132,92 @@ class Dml {
 	}
 
 	/**
-	 * @description A convenience method for creating left joins. Joins will be added to the join array and processed
+	 * @description A convenience method for creating left joins. Joins will be added to the join array and processed 
 	 * 		in their array order and use the ON syntax rather than USING. Optional for query build.
-	 * @param string $table - The name of the table to be joined with the current table
-	 * @param string $column - The column(s) to be compared to the value
-	 * @param string $operator - The operator to be used in the comparison
-	 * @param string $value - The value to which $column is compared
-	 * @param string $tableAlias - optional - The alias of the joined table. If not set, alias defaults to the table name
+	 * @param string $finalTable - The name of the table to be joined with the current table. For 2 table joins, this 
+	 * 		will simply be the second table. For 3 table joins, it will be the third table, which is joined to the 
+	 * 		current table through an intermediary table.
+	 * @param string $throughTable - optional - The name of the intermediary table in a 3 table join. Do not use for a
+	 * 		2 table join.
 	 * @return The object for chaining
-	 */
-	public function leftJoin($table, $column, $operator, $value, $tableAlias=NULL){
-		$this->driver->leftJoin($table, $column, $operator, $value, $tableAlias);
+	 * @note In order to use the leftJoin method, your database must follow a strict naming pattern. 
+	 * 		Database tables must named using plural forms of whatever nouns they represent. Foreign keys refering to
+	 * 		the id of any table must be camel-cased and use the singular form of the noun. So, for a table named 
+	 * 		'users', any foreign key reffering to the users table must be named 'userId'. <strong>Because VM PHP
+	 * 		Framework does not contain a pluralization dictionary, if your noun has irregular pluralization, ie. 
+	 * 		'person/people', you will not be able to use the leftJoin method and should instead construct your query 
+	 * 		manually using the join method.</strong>
+	 */	
+	public function leftJoin($finalTable, $throughTable = NULL){
+		$this->driver->leftJoin($finalTable, $throughTable);
 		return $this;
 	}
-	
+
+	/**
+	 * @description A convenience method for creating right joins. Joins will be added to the join array and processed 
+	 * 		in their array order and use the ON syntax rather than USING. Optional for query build.
+	 * @param string $finalTable - The name of the table to be joined with the current table. For 2 table joins, this 
+	 * 		will simply be the second table. For 3 table joins, it will be the third table, which is joined to the 
+	 * 		current table through an intermediary table.
+	 * @param string $throughTable - optional - The name of the intermediary table in a 3 table join. Do not use for a
+	 * 		2 table join.
+	 * @return The object for chaining
+	 * @note In order to use the rightJoin method, your database must follow a strict naming pattern. 
+	 * 		Database tables must named using plural forms of whatever nouns they represent. Foreign keys refering to
+	 * 		the id of any table must be camel-cased and use the singular form of the noun. So, for a table named 
+	 * 		'users', any foreign key reffering to the users table must be named 'userId'. <strong>Because VM PHP
+	 * 		Framework does not contain a pluralization dictionary, if your noun has irregular pluralization, ie. 
+	 * 		'person/people', you will not be able to use the rightJoin method and should instead construct your query 
+	 * 		manually using the join method.</strong>
+	 */	
+	public function rightJoin($finalTable, $throughTable = NULL){
+		$this->driver->rightJoin($finalTable, $throughTable);
+		return $this;
+	}	
+
 	/**
 	 * @description A convenience method for creating inner joins. Joins will be added to the join array and processed
 	 * 		in their array order and use the ON syntax rather than USING. Optional for query build.
-	 * @param string $table - The name of the table to be joined with the current table
-	 * @param string $column - The column(s) to be compared to the value
-	 * @param string $operator - The operator to be used in the comparison
-	 * @param string $value - The value to which $column is compared
-	 * @param string $tableAlias - optional - The alias of the joined table. If not set, alias defaults to the table name
+	 * @param string $finalTable - The name of the table to be joined with the current table. For 2 table joins, this
+	 * 		will simply be the second table. For 3 table joins, it will be the third table, which is joined to the
+	 * 		current table through an intermediary table.
+	 * @param string $throughTable - optional - The name of the intermediary table in a 3 table join. Do not use for a
+	 * 		2 table join.
 	 * @return The object for chaining
+	 * @note In order to use the innerJoin method, your database must follow a strict naming pattern.
+	 * 		Database tables must named using plural forms of whatever nouns they represent. Foreign keys refering to
+	 * 		the id of any table must be camel-cased and use the singular form of the noun. So, for a table named
+	 * 		'users', any foreign key reffering to the users table must be named 'userId'. <strong>Because VM PHP
+	 * 		Framework does not contain a pluralization dictionary, if your noun has irregular pluralization, ie.
+	 * 		'person/people', you will not be able to use the innerJoin method and should instead construct your query
+	 * 		manually using the join method.</strong>
 	 */
-	public function innerJoin($table, $column, $operator, $value, $tableAlias=NULL){
-		$this->driver->innerJoin($table, $column, $operator, $value, $tableAlias);
+	public function innerJoin($finalTable, $throughTable = NULL){
+		$this->driver->innerJoin($finalTable, $throughTable);
 		return $this;
-	}
-	
+	}	
+
 	/**
 	 * @description A convenience method for creating full joins. Joins will be added to the join array and processed
 	 * 		in their array order and use the ON syntax rather than USING. Optional for query build.
-	 * @param string $table - The name of the table to be joined with the current table
-	 * @param string $column - The column(s) to be compared to the value
-	 * @param string $operator - The operator to be used in the comparison
-	 * @param string $value - The value to which $column is compared
-	 * @param string $tableAlias - optional - The alias of the joined table. If not set, alias defaults to the table name
+	 * @param string $finalTable - The name of the table to be joined with the current table. For 2 table joins, this
+	 * 		will simply be the second table. For 3 table joins, it will be the third table, which is joined to the
+	 * 		current table through an intermediary table.
+	 * @param string $throughTable - optional - The name of the intermediary table in a 3 table join. Do not use for a
+	 * 		2 table join.
 	 * @return The object for chaining
+	 * @note In order to use the fullJoin method, your database must follow a strict naming pattern.
+	 * 		Database tables must named using plural forms of whatever nouns they represent. Foreign keys refering to
+	 * 		the id of any table must be camel-cased and use the singular form of the noun. So, for a table named
+	 * 		'users', any foreign key reffering to the users table must be named 'userId'. <strong>Because VM PHP
+	 * 		Framework does not contain a pluralization dictionary, if your noun has irregular pluralization, ie.
+	 * 		'person/people', you will not be able to use the fullJoin method and should instead construct your query
+	 * 		manually using the join method.</strong>
 	 */
-	public function fullJoin($table, $column, $operator, $value, $tableAlias=NULL){
-		$this->driver->fullJoin($table, $column, $operator, $value, $tableAlias);
+	public function fullJoin($finalTable, $throughTable = NULL){
+		$this->driver->fullJoin($finalTable, $throughTable);
 		return $this;
 	}
-	
-	/**
-	 * @description A convenience method for creating right joins. Joins will be added to the join array and processed
-	 * 		in their array order and use the ON syntax rather than USING. Optional for query build.
-	 * @param string $table - The name of the table to be joined with the current table
-	 * @param string $column - The column(s) to be compared to the value
-	 * @param string $operator - The operator to be used in the comparison
-	 * @param string $value - The value to which $column is compared
-	 * @param string $tableAlias - optional - The alias of the joined table. If not set, alias defaults to the table name
-	 * @return The object for chaining
-	 */
-	public function rightJoin($table, $column, $operator, $value, $tableAlias=NULL){
-		$this->driver->rightJoin($table, $column, $operator, $value, $tableAlias);
-		return $this;
-	}	
 	
 	/**
 	 * @description Creates a where clause.  Optional for query build.
@@ -267,6 +320,84 @@ class Dml {
  		$this->driver->clear($clearBound);
 	} 
 
+	/**
+	 * @description Gets the fields from joined tables and prepends them with the table name. Fields in the current
+	 * 		table will not be prepended.
+	 * @param array $tables - The table names to be joined to the current table, either as keys or values or both
+	 * @return The object for chaining.
+	 * @note The autoSelectList method will automatically change the names of fields from joined tables in order to
+	 * 		prevent overwriting the current table's fields. The name of the table will be made singular and prepended
+	 * 		to the name of the field. The composite name will be camel-cased. For example, if <i>users</i> is the current
+	 * 		table and it is joined to <i>pages</i> table, pages.id will aliased as pageId, pages.name will be pageName,
+	 * 		and so on. Any fields in the users table will not be aliased, so users.id will simply be id, users.name will
+	 * 		remain name, etc.
+	 */
+	public function autoSelectList(array $tables){
+		$this->driver->autoSelectList($tables);
+		return $this;
+	}	
+	
+	/**
+	 * @description A shortcut method for quickly retrieving data from the table
+	 * @param mixed $values - Either a single value or an array of values to be found in the specified column.
+	 * @param string $column - optional - The name of the column for which data should be matched.  The column must be 
+	 * 		present in the table or else an exception will be thrown. Defaults to 'id'.
+	 * @param array $joins - optional - An array of table names that should be joined to the returned result. For 
+	 * 		simple 2 table joins, enter the joined table name as an array value. If you have a 3 table join with an 
+	 * 		intermediary table between the current table and the final table, enter the intermediary table as the array 
+	 * 		key and the final table as the array value. See the attached note for important information on naming 
+	 * 		conventions.
+	 * @return The object for chaining.
+	 * @note In order to use the joins parameter of the find method, your database must follow a strict naming pattern. 
+	 * 		Database tables must named using plural forms of whatever nouns they represent. Foreign keys refering to
+	 * 		the id of any table must be camel-cased and use the singular form of the noun. So, for a table named 
+	 * 		'users', any foreign key reffering to the users table must be named 'userId'. <strong>Because VM PHP
+	 * 		Framework does not contain a pluralization dictionary, if your noun has irregular pluralization, ie. 
+	 * 		'person/people', you will not be able to use the find method and should instead construct your query 
+	 * 		manually.</strong>
+	 * @note Because the find method is merely a shortcut method that dynamically creates where and join clauses for 
+	 * 		simple queries, you can use other methods for any of the other SQL clauses before using the find method to 
+	 * 		further refine your queries. The sole exception to this is that you will not be able to use the alias 
+	 * 		method or any alias parameters as find relies on full table names rather than aliases.
+	 * @note The joins parameter is only for use with the select method and will be ignored when using the find method 
+	 * 		in conjuction with delete or update methods.
+	 * @note When using the joins parameter, keep in mind that any field names from the joined tables will be altered 
+	 * 		in the result set returned from the query. find will automatically run autoSelectList if needed. See the 
+	 * 		note on the autoSelectList method for more details.
+	 */
+	public function find($values, $column = 'id', array $joins = array()){
+		return $this->driver->find($values, $column, $joins);
+		return $this;
+	}	
+
+	/**
+	 * @description Retrieves the first row in the table when sorted by id.
+	 * @param string $mode - optional - Corresponds to the mode parameter in the select method. See that method for more
+	 * 		details. Defaults to 'single'.
+	 * @return mixed - The query result set as detailed in the select method.
+	 */
+	public function first($mode = 'single'){
+		return $this->driver->first($mode);
+	}
+	
+	/**
+	 * @description Retrieves the last row in the table when sorted by id.
+	 * @param string $mode - optional - Corresponds to the mode parameter in the select method. See that method for more
+	 * 		details. Defaults to 'single'.
+	 * @return mixed - The query result set as detailed in the select method.
+	 */
+	public function last($mode = 'single'){
+		return $this->driver->last($mode);
+	}
+	
+	/**
+	 * @description A convenience method for retrieving the number of records. Is equivalent to select('count').
+	 * @returns int - The number of records for the given query.
+	 */
+	public function count(){
+		return $this->driver->count();
+	}	
+	
 	/**
 	 * @description Compiles the given data into a select query and returns a result set based on the query
 	 * @param string (optional) $mode - 
